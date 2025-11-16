63c9f9f4b174e472a7a238713e4ee2a2950a79ca
wein1m <weynn.sz@gmail.com>
2025-11-16 20:58:16 +0800

    Configure logger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(
+  morgan('combined', { stream: { write: msg => logger.info(msg.trim()) } })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hellowww~');
+
   res.status(200).send('Yokosoo~');
+});
+
+app.get('/api/status', (req, res) => {
+  logger.info('API Status: Okieee~');
+
+  res.status(200).send({
+    status: 'UP',
+    message: 'The API is up and running perfectly fine!!!!!!',
+  });
 });
 
 export default app;
